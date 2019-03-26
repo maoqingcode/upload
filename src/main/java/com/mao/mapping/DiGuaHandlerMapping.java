a793589f4f10e3308651dcf4d54e15da3efa5021
maoqing <maoqingsingle@163.com>
2019-03-26 11:18:30 +0800

    测试上传

--- src/main/java/com/mao/mapping/DiGuaHandlerMapping.java
+++ src/main/java/com/mao/mapping/DiGuaHandlerMapping.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 
 public class DiGuaHandlerMapping implements HandlerMapping {
 
-    @Override
+
     public HandlerExecutionChain getHandler(HttpServletRequest request) throws Exception {
-        String url=request.getRequestURI().toString();
-        String method=request.getMethod();
-        if(url.startsWith("/tudou")){
-            if(method.equalsIgnoreCase("GET")){
-                //返回卖地瓜的handler伪代码
-               // return "salle digua";
-            }
-        }
+//        String url=request.getRequestURI().toString();
+//        String method=request.getMethod();
+//        if(url.startsWith("/tudou")){
+//            if(method.equalsIgnoreCase("GET")){
+//                //返回卖地瓜的handler伪代码
+//               // return "salle digua";
+//            }
+//        }
         return null;
     }
 }
